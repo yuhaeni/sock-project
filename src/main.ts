53cab50e0d00f672988d7fa5087cccec223b504a
haeni311 <haeni311@gmail.com>
2023-03-09 18:03:52 +0900

    EventsGateway 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.useWebSocketAdapter(new IoAdapter(app));
 }
 bootstrap();
